4d7a3a3b2d5a9eed897bd00a8af0ea7b568e947d
unknown <goliveiramisael18@gmail.com>
2024-09-04 22:26:25 -0300

    php

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Http\Controllers\ProdutosControler;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/cadastro_usuario', [UserController::class,"formCriarUsuario"]);
 
-Route::get('/users', [UserController::class, 'listar']);
+Route::get('/listar_usuario', [UserController::class, 'listar']);
 
-Route::post('/users', [UserController::class, 'criar']);
+Route::post('/criar_usuario', [UserController::class, 'criar']);
+
+Route::post('/cadastro_produto', [ProdutosControler::class,'']);
